155580262e601da7524de56b394f29786aa7df74
hypergalois <jose.delgado2@live.u-tad.com>
2025-03-15 10:32:18 +0100

    Add welcome image and implement login button in Welcome component

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,11 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import StyledText from "@/components/StyledText";
 import { colors, spacingX, spacingY } from "@/constants/theme";
 import { verticalScale } from "@/utils/styling";
+import images from "@/constants/images";
 
 const Welcome = () => {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <View>
+        <TouchableOpacity style={styles.loginButton}>
+          <StyledText
+            weight={"Medium"}
+            style={{ textDecorationLine: "underline" }}
+          >
+            Sign In
+          </StyledText>
+        </TouchableOpacity>
+
+        <Image
+          source={images.welcome}
+          style={styles.welcomeImage}
+          resizeMode="contain"
+        />
+      </View>
+    </View>
+  );
 };
 
 export default Welcome;
