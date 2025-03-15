3e943d12f23f0ace3a4667bc0caca2a24f1973de
hypergalois <jose.delgado2@live.u-tad.com>
2025-03-15 10:00:24 +0100

    Add font constants for Kanit typeface

---
+++ constants/fonts.ts
@@ -0,0 +1,22 @@
+const Fonts = {
+  KanitBlack: "Kanit-Black",
+  KanitBlackItalic: "Kanit-BlackItalic",
+  KanitBold: "Kanit-Bold",
+  KanitBoldItalic: "Kanit-BoldItalic",
+  KanitExtraBold: "Kanit-ExtraBold",
+  KanitExtraBoldItalic: "Kanit-ExtraBoldItalic",
+  KanitExtraLight: "Kanit-ExtraLight",
+  KanitExtraLightItalic: "Kanit-ExtraLightItalic",
+  KanitItalic: "Kanit-Italic",
+  KanitLight: "Kanit-Light",
+  KanitLightItalic: "Kanit-LightItalic",
+  KanitMedium: "Kanit-Medium",
+  KanitMediumItalic: "Kanit-MediumItalic",
+  KanitRegular: "Kanit-Regular",
+  KanitSemiBold: "Kanit-SemiBold",
+  KanitSemiBoldItalic: "Kanit-SemiBoldItalic",
+  KanitThin: "Kanit-Thin",
+  KanitThinItalic: "Kanit-ThinItalic",
+};
+
+export default Fonts;
