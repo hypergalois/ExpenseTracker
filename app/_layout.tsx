dcde22a44fddd0798aef5392dfd402b87602443a
hypergalois <jose.delgado2@live.u-tad.com>
2025-03-15 08:57:26 +0100

    Refactor authentication layout and index components for improved structure and readability

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,58 +1,11 @@
-import { Stack, useRouter, useSegments } from "expo-router";
-import { useEffect, useState } from "react";
-import User, { getAuth, onAuthStateChanged } from "@react-native-firebase/auth";
-import { View, ActivityIndicator } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState<typeof User | null>(null);
-  const router = useRouter();
-  const segments = useSegments();
+const RootLayout = () => {
+  return <Stack screenOptions={{ headerShown: false }}></Stack>;
+};
 
-  useEffect(() => {
-    const authInstance = getAuth();
+export default RootLayout;
 
-    const unsubscribe = onAuthStateChanged(
-      authInstance,
-      (user: typeof User) => {
-        console.log("onAuthStateChanged", user);
-        setUser(user);
-        if (initializing) setInitializing(false);
-      }
-    );
-
-    return unsubscribe;
-  }, []);
-
-  useEffect(() => {
-    if (initializing) return;
-
-    const inAuthGroup = segments[0] === "(auth)";
-
-    if (user && !inAuthGroup) {
-      router.replace("/(auth)/home");
-    } else if (!user && inAuthGroup) {
-      router.replace("/");
-    }
-  }, [user, initializing]);
-
-  if (initializing)
-    return (
-      <View
-        style={{
-          alignItems: "center",
-          justifyContent: "center",
-          flex: 1,
-        }}
-      >
-        <ActivityIndicator size="large" />
-      </View>
-    );
-
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Login" }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-    </Stack>
-  );
-}
+const styles = StyleSheet.create({});
