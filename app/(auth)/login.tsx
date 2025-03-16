8fd4d1871deb21bcc5dedb0a8deec1351ddc9a12
hypergalois <jose.delgado2@live.u-tad.com>
2025-03-16 07:41:52 +0100

    Refactor BackButton component to use TouchableOpacity and implement navigation functionality

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React from "react";
 import { colors, spacingX, spacingY } from "@/constants/theme";
 import { verticalScale } from "@/utils/styling";
