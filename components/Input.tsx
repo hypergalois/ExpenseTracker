0ab89cc4388041e97d5d48c30c07cb49cd38ba3d
hypergalois <jose.delgado2@live.u-tad.com>
2025-03-16 08:20:00 +0100

    Enhance Input component with TextInput, styling, and props support

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,14 +1,41 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import React from "react";
+import { InputProps } from "@/types";
+import { colors, radius, spacingX } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
 
-const Input = () => {
+const Input = ({ icon, containerStyle, inputStyle, inputRef }: InputProps) => {
   return (
-    <View>
-      <Text>Input</Text>
+    <View style={[styles.container, containerStyle]}>
+      {icon}
+      <TextInput
+        style={[styles.input, inputStyle]}
+        ref={inputRef}
+        placeholderTextColor={colors.neutral400}
+        placeholder="Email"
+      />
     </View>
   );
 };
 
 export default Input;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    height: verticalScale(54),
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: colors.neutral300,
+    borderRadius: radius._12,
+    borderCurve: "continuous",
+    paddingHorizontal: spacingX._15,
+    gap: spacingX._10,
+  },
+  input: {
+    flex: 1,
+    fontSize: verticalScale(16),
+    color: colors.white,
+  },
+});
