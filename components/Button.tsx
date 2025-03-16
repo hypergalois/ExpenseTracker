ffaf77bae7aeecbe20be497a8dfeec04d1900c36
hypergalois <jose.delgado2@live.u-tad.com>
2025-03-16 06:13:39 +0100

    Enhance Button component with loading state and add new Loading component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
 import React from "react";
 import { CustomButtonProps } from "@/types";
+import { colors, radius } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
 
 const Button = ({
   style,
@@ -8,13 +10,30 @@ const Button = ({
   loading = false,
   children,
 }: CustomButtonProps) => {
+  if (loading) {
+    return (
+      <View
+        style={[styles.button, style, { backgroundColor: "transparent" }]}
+      ></View>
+    );
+  }
+
   return (
-    <View>
-      <Text>Button</Text>
-    </View>
+    <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
+      {children}
+    </TouchableOpacity>
   );
 };
 
 export default Button;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.primary,
+    borderRadius: radius._17,
+    borderCurve: "continuous",
+    height: verticalScale(50),
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
